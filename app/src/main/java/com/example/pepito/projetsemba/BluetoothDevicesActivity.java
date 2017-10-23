3c5a8cc62c7629e851085f30c41810ebca96ce05
MuchoPepito <declerck.jessy@gmail.com>
2017-10-23 17:55:51 +0200

    Bluetooth discovery works

--- app/src/main/java/com/example/pepito/projetsemba/BluetoothDevicesActivity.java
+++ app/src/main/java/com/example/pepito/projetsemba/BluetoothDevicesActivity.java
@@ -1,19 +1,35 @@
 package com.example.pepito.projetsemba;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class BluetoothDevicesActivity extends AppCompatActivity {
 
     final String tag = MainActivity.class.getName();
     protected List<String> dnList;
-    protected ListView pdnListView;
+    protected ListView dnListView;
     protected ArrayAdapter<String> dnArrayAdapter;
+
+    protected BluetoothAdapter bTAdapter;
+
+
+    public final static int REQUEST_BLUETOOTH = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +37,50 @@ public class BluetoothDevicesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_bluetooth_devices);
 
         dnList = new ArrayList<>();
-        dnList.add("periph 1");
-        dnList.add("periph 2");
-        dnList.add("periph 3");
-        dnList.add("periph 4");
 
-        pdnListView = (ListView) findViewById(R.id.bluetoothDevices);
+        dnListView = (ListView) findViewById(R.id.bluetoothDevices);
         dnArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dnList);
-        pdnListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-        pdnListView.setAdapter(dnArrayAdapter);
-        
+        dnListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        dnListView.setAdapter(dnArrayAdapter);
+
+
+        bTAdapter = BluetoothAdapter.getDefaultAdapter();
+        // Phone does not support Bluetooth so let the user know and exit.
+        if (bTAdapter == null) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Not compatible")
+                    .setMessage("Your phone does not support Bluetooth")
+                    .setPositiveButton("Exit", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            System.exit(0);
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }
+
+        if (!bTAdapter.isEnabled()) {
+            Intent enableBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBT, REQUEST_BLUETOOTH);
+        }
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        dnArrayAdapter.clear();
+        registerReceiver(bReceiver, filter);
+        bTAdapter.startDiscovery();
+
     }
+
+
+    private final BroadcastReceiver bReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+                dnList.add(device.getName());
+                dnArrayAdapter.notifyDataSetChanged();
+            }
+        }
+    };
 }
--- app/src/main/java/com/example/pepito/projetsemba/MainActivity.java
+++ app/src/main/java/com/example/pepito/projetsemba/MainActivity.java
@@ -1,11 +1,21 @@
 package com.example.pepito.projetsemba;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,18 +26,23 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     final String tag = MainActivity.class.getName();
     protected List<String> fnList;
     protected ListView fnListView;
     protected ArrayAdapter<String> fnArrayAdapter;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
         fnList = new ArrayList<>();
         fnList.add("fichier 1");
@@ -65,6 +80,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
